b64a0123d2b5dff8cb039d4ec2f1d22332b1dc48
Pradnya Mane <pradnya.mane9920@gmail.com>
2022-02-09 16:43:17 +0530

    Adding package

---
+++ RPM-EcommercePortal/src/main/java/com/cts/ecommerce/RpmEcommercePortalApplication.java
@@ -0,0 +1,13 @@
+package com.cts.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpmEcommercePortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpmEcommercePortalApplication.class, args);
+	}
+
+}
