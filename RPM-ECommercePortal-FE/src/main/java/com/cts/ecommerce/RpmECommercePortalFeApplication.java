c3793b12ba5f6d1a9ee0b11eac83bd25a8bad96f
2065731 <2065731@host.docker.internal>
2022-02-09 16:46:11 +0530

    created frontend file

---
+++ RPM-ECommercePortal-FE/src/main/java/com/cts/ecommerce/RpmECommercePortalFeApplication.java
@@ -0,0 +1,13 @@
+package com.cts.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpmECommercePortalFeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpmECommercePortalFeApplication.class, args);
+	}
+
+}
