8162336c5761577038466fb081d7717ffb05e8c0
MMaharshiReddy <98950967+MMaharshiReddy@users.noreply.github.com>
2022-02-09 16:11:47 +0530

    Created vendor module,vendor microservice class

---
+++ RPM-VendorMicroservice/src/main/java/com/cts/microservice/RpmVendorMicroserviceApplication.java
@@ -0,0 +1,21 @@
+package com.cts.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+import lombok.extern.slf4j.Slf4j;
+
+@SpringBootApplication
+@EnableEurekaClient
+@Slf4j
+public class RpmVendorMicroserviceApplication {
+
+	public static void main(String[] args) {
+
+			log.info("Main started...");
+			SpringApplication.run(RpmVendorMicroserviceApplication.class, args);
+			log.info("Main ended...");
+	}
+
+}
