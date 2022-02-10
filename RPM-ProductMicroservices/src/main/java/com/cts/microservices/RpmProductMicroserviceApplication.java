44cd0e15ccf1cef7ee09e87a13d4108787c7829f
Shubhs143 <sj143418@gmail.com>
2022-02-10 17:10:56 +0530

    Created project and created entity

--- RPM-ProductMicroservices/src/main/java/com/cts/microservices/RpmProductMicroserviceApplication.java
+++ RPM-ProductMicroservices/src/main/java/com/cts/microservices/RpmProductMicroserviceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RpmProductMicroservicesApplication {
+public class RpmProductMicroserviceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RpmProductMicroservicesApplication.class, args);
+		SpringApplication.run(RpmProductMicroserviceApplication.class, args);
 	}
 
 }
